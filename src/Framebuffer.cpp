b9631bd061646261d9c72904e0c856018fdfb00a
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-11-09 23:14:19 +0100

    [WIP] Set up SetPixel in Framebuffer.cpp

--- src/Framebuffer.cpp
+++ src/Framebuffer.cpp
@@ -6,3 +6,13 @@ Framebuffer::Framebuffer(int width, int height) : m_width(width), m_height(heigh
     // Without this, m_pixels would be empty (size=0) and accessing any index would crash
     m_pixels.resize(width * height, {0, 0, 0, 255});
 }
+
+Framebuffer::SetPixel(int x, int y, const Color &color)
+{
+    // Check if x, y is within bounds
+    if (x < 0 || x >= m_width || y < 0 || y >= m_height)
+        return;
+
+    int index = y * m_width + x;
+    m_pixels[index] = color;
+}
