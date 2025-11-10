3f0c16f1f2ff902d75dc53146722bc6e7428f490
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-11-10 21:57:32 +0100

    [WIP] Implement Clear functionality to clear pixels

--- src/Framebuffer.cpp
+++ src/Framebuffer.cpp
@@ -7,7 +7,7 @@ Framebuffer::Framebuffer(int width, int height) : m_width(width), m_height(heigh
     m_pixels.resize(width * height, {0, 0, 0, 255});
 }
 
-Framebuffer::SetPixel(int x, int y, const Color &color)
+void Framebuffer::SetPixel(int x, int y, const Color &color)
 {
     // Check if x, y is within bounds
     if (x < 0 || x >= m_width || y < 0 || y >= m_height)
@@ -16,4 +16,13 @@ Framebuffer::SetPixel(int x, int y, const Color &color)
     // Map 2D in 1D Vector
     int index = y * m_width + x;
     m_pixels[index] = color;
+}
+
+void Framebuffer::Clear(const Color &color)
+{
+    // Loop through each pixel and clear it by setting it to a certain color
+    for (auto &pixel : m_pixels)
+    {
+        pixel = color;
+    }
 }
